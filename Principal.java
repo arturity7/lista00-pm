ab3c2e305cc7f2b644fac448460d4c8a6839a364
arturity7 <gabrielarthur730@gmail.com>
2026-08-10 18:38:51 -0300

    Atividade 2 - Maior e Menor

--- Principal.java
+++ Principal.java
@@ -2,20 +2,69 @@ import java.util.Scanner;
 
 public class Principal {
     public static void main(String[] args) {
-        Scanner inteiro = new Scanner(System.in);
-        System.out.print("Digite um número: ");
-        int x = inteiro.nextInt();
+        Scanner number = new Scanner(System.in);
 
-        if (x < 0) {
-            System.out.println("Fatorial não existe para número negativo.");
-        } else {
-            long fatorial = 1;
-            for (int i = 2; i <= x; i++) {
-                fatorial *= i;
-            }
-            System.out.println(x + "! = " + fatorial);
+       
+        int[] valores = new int[3];
+        System.out.println("Digite 3 números: ");
+        for (int i = 0; i < valores.length; i++) {
+            valores[i] = number.nextInt();
         }
 
-        inteiro.close();
+        int x = valores[0];
+        int y = valores[1];
+        int z = valores[2];
+
+       
+        int maior;
+        if (x >= y && x >= z) {
+            maior = x;
+        } else if (y >= x && y >= z) {
+            maior = y;
+        } else {
+            maior = z;
+        }
+
+        
+        int menor;
+        if (x <= y && x <= z) {
+            menor = x;
+        } else if (y <= x && y <= z) {
+            menor = y;
+        } else {
+            menor = z;
+        }
+
+        System.out.println("O maior número é o: " + maior);
+        System.out.println("O menor número é o: " + menor);
+
+        
+        int limiteInferior;
+        int limiteSuperior;
+
+        if (y <= z) {
+            limiteInferior = y;
+            limiteSuperior = z;
+        } else {
+            limiteInferior = z;
+            limiteSuperior = y;
+        }
+
+        if (x >= limiteInferior && x <= limiteSuperior) {
+            System.out.println("O X está dentro do intervalo [" + y + ", " + z + "]");
+        } else {
+            System.out.println("O X está fora do intervalo [" + y + ", " + z + "]");
+        }
+
+        
+        if (y == 0 || z == 0) {
+            System.out.println("Não dá para testar divisibilidade: divisão por zero");
+        } else if (x % y == 0 && x % z == 0) {
+            System.out.println("X é divisível por Y e por Z");
+        } else {
+            System.out.println("X não é divisível por Y e por Z");
+        }
+
+        number.close();
     }
 }
