f055504b59956f484018d07520a68b8ef490cb33
arturity7 <gabrielarthur730@gmail.com>
2026-08-19 16:39:13 -0300

    Atividade 7 - meses

--- Principal.java
+++ Principal.java
@@ -1,44 +1,39 @@
 import java.util.Scanner;
 
 public class Principal {
+
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        String[] gabarito = new String[8];
+        String[] meses = { "janeiro", "fevereiro", "marco", "abril", "maio", "junho",
+                "julho", "agosto", "setembro", "outubro", "novembro", "dezembro" };
 
-        System.out.println("Digite o gabarito da prova (8 respostas): ");
-        for (int i = 0; i < 8; i++) {
-            System.out.println("Resposta da questao " + (i + 1) + ": ");
-            gabarito[i] = input.next();
+        double[] ano = new double[12];
+
+        for (int i = 0; i < 12; i++) {
+            System.out.println("Digite a temperatura media de " + meses[i] + ": ");
+            ano[i] = input.nextDouble();
         }
 
-        int aprovados = 0;
+        double maior = ano[0];
+        int mesMaior = 0;
+        double menor = ano[0];
+        int mesMenor = 0;
 
-        for (int aluno = 0; aluno < 10; aluno++) {
-            System.out.println("Digite o numero do aluno: ");
-            int numero = input.nextInt();
-
-            int nota = 0;
-
-            for (int i = 0; i < 8; i++) {
-                System.out.println("Resposta do aluno para a questao " + (i + 1) + ": ");
-                String resposta = input.next();
-
-                if (resposta.equalsIgnoreCase(gabarito[i])) {
-                    nota++;
-                }
+        for (int i = 1; i < 12; i++) {
+            if (ano[i] > maior) {
+                maior = ano[i];
+                mesMaior = i;
             }
 
-            if (nota >= 6) {
-                aprovados++;
+            if (ano[i] < menor) {
+                menor = ano[i];
+                mesMenor = i;
             }
-
-            System.out.println("Aluno: " + numero + " - Nota: " + nota);
         }
 
-        double porcentagem = (aprovados * 100.0) / 10;
-
-        System.out.println("Porcentagem de aprovacao: " + porcentagem + "%");
+        System.out.println("Maior temperatura: " + maior + " - mes: " + meses[mesMaior]);
+        System.out.println("Menor temperatura: " + menor + " - mes: " + meses[mesMenor]);
 
         input.close();
     }
